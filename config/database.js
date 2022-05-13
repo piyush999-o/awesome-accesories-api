6fa3af188fd06389b0e3f076f3fc30373d73ba0b
Piyush Tanti <magicianworld99@gmail.com>
2022-05-13 15:17:28 +0530

    Initial Commit

---
+++ config/database.js
@@ -0,0 +1,27 @@
+// strapi-api/config/database.js
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "products"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "piyush99"),
+      schema: env("DATABASE_SCHEMA", "public"),
+    },
+  }
+});
+
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
